8772fc1645483bae2535f695ea22c6bb4f965187
maxenceboo <maxenceboo@gmail.com>
2022-04-20 09:30:36 +0200

    add comment

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
-import axios from "axios"
+import axios from "axios" // axios is a promise based HTTP client for the browser and node.js
 
-export default axios.create({
-    baseURL: "http://vps-e1a777fe.vps.ovh.net:3003/api",
+export default axios.create({ // axios.create() returns a new instance of axios
+    baseURL: "http://vps-e1a777fe.vps.ovh.net:3003/api", // set the baseURL to the api
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
